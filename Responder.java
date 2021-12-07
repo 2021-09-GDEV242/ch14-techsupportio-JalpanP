85d47fa8e4b99b9a924cbd05d498af023fcd7a47
JalpanP <G00268928@stu.raritanval.edu>
2021-12-06 22:14:17 -0500

    added to the fillResponseMap

--- Responder.java
+++ Responder.java
@@ -25,6 +25,7 @@ public class Responder
     private ArrayList<String> defaultResponses;
     // The name of the file containing the default responses.
     private static final String FILE_OF_DEFAULT_RESPONSES = "default.txt";
+    private static final String FILE_OF_RESPONSE_MAP = "responses.txt";
     private Random randomGenerator;
 
     /**
@@ -67,52 +68,67 @@ public class Responder
      */
     private void fillResponseMap()
     {
-        responseMap.put("crash", 
-                        "Well, it never crashes on our system. It must have something\n" +
-                        "to do with your system. Tell me more about your configuration.");
-        responseMap.put("crashes", 
-                        "Well, it never crashes on our system. It must have something\n" +
-                        "to do with your system. Tell me more about your configuration.");
-        responseMap.put("slow", 
-                        "I think this has to do with your hardware. Upgrading your processor\n" +
-                        "should solve all performance problems. Have you got a problem with\n" +
-                        "our software?");
-        responseMap.put("performance", 
-                        "Performance was quite adequate in all our tests. Are you running\n" +
-                        "any other processes in the background?");
-        responseMap.put("bug", 
-                        "Well, you know, all software has some bugs. But our software engineers\n" +
-                        "are working very hard to fix them. Can you describe the problem a bit\n" +
-                        "further?");
-        responseMap.put("buggy", 
-                        "Well, you know, all software has some bugs. But our software engineers\n" +
-                        "are working very hard to fix them. Can you describe the problem a bit\n" +
-                        "further?");
-        responseMap.put("windows", 
-                        "This is a known bug to do with the Windows operating system. Please\n" +
-                        "report it to Microsoft. There is nothing we can do about this.");
-        responseMap.put("macintosh", 
-                        "This is a known bug to do with the Mac operating system. Please\n" +
-                        "report it to Apple. There is nothing we can do about this.");
-        responseMap.put("expensive", 
-                        "The cost of our product is quite competitive. Have you looked around\n" +
-                        "and really compared our features?");
-        responseMap.put("installation", 
-                        "The installation is really quite straight forward. We have tons of\n" +
-                        "wizards that do all the work for you. Have you read the installation\n" +
-                        "instructions?");
-        responseMap.put("memory", 
-                        "If you read the system requirements carefully, you will see that the\n" +
-                        "specified memory requirements are 1.5 giga byte. You really should\n" +
-                        "upgrade your memory. Anything else you want to know?");
-        responseMap.put("linux", 
-                        "We take Linux support very seriously. But there are some problems.\n" +
-                        "Most have to do with incompatible glibc versions. Can you be a bit\n" +
-                        "more precise?");
-        responseMap.put("bluej", 
-                        "Ahhh, BlueJ, yes. We tried to buy out those guys long ago, but\n" +
-                        "they simply won't sell... Stubborn people they are. Nothing we can\n" +
-                        "do about it, I'm afraid.");
+        Charset charset = Charset.forName("US-ASCII");
+        Path path = Paths.get(FILE_OF_RESPONSE_MAP);
+        try (BufferedReader read = Files.newBufferedReader(path, charset)) 
+        {
+            String response = read.readLine();
+            
+            while(response != null)
+            {
+                ArrayList<String> keyList = new ArrayList<String>();
+                String key = "";
+                String res = "";
+                
+                boolean keys = true;
+                
+                int i;
+                int stop = 0;
+                
+                while(keys)
+                {
+                    for(i = stop; i < response.indexOf(",", stop); i++)
+                    {
+                        key += response.charAt(i);
+                    }
+                    stop = i;
+                    if(stop == response.length())
+                    {
+                        keys = false;
+                    }
+                    else
+                    {
+                        stop++;
+                        keyList.add(key);
+                        key = "";
+                    }
+                }
+                response = read.readLine();
+                
+                while(response != null && !response.equals(""))
+                {
+                    res += response;
+                    response = read.readLine();
+                }
+                for(int t = 0; t < keyList.size(); t++)
+                {
+                    responseMap.put(keyList.get(t), res);
+                
+                }
+                response = read.readLine();
+            }
+        }
+        
+        catch(FileNotFoundException e) 
+        {
+            System.err.println("File not Responding, Unable to Open" + FILE_OF_RESPONSE_MAP);
+            
+        }
+        catch(IOException e)
+        {
+            System.err.println(" A Problem Occured When Reading the File" + FILE_OF_RESPONSE_MAP);
+            
+        }
     }
 
     /**
@@ -124,10 +140,43 @@ public class Responder
         Charset charset = Charset.forName("US-ASCII");
         Path path = Paths.get(FILE_OF_DEFAULT_RESPONSES);
         try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
+            BufferedReader reader2 = Files.newBufferedReader(path, charset);
             String response = reader.readLine();
-            while(response != null) {
-                defaultResponses.add(response);
+            
+            String ahead = reader2.readLine();
+            String combo = "";
+            ahead = reader2.readLine();
+            
+            while(response != null && ahead != null) 
+            {
+                if(combo == "" && response.equals("") == false && ahead.equals(""))
+                {
+                    defaultResponses.add(response);
+                }
+                else if(combo != "" && ahead.equals(""))
+                {
+                    defaultResponses.add(combo);
+                    combo = "";
+                }
+                else if(response.equals("") == false && ahead.equals("") == false)
+                {
+                    if(combo == "")
+                    {
+                        combo = response + ahead;
+                    }
+                    else 
+                    {
+                        combo = combo + ahead;
+                    }
+                }
                 response = reader.readLine();
+                ahead = reader2.readLine();
+                
+            }
+            if(response.equals("") == false)
+            {
+                defaultResponses.add(response);
+                System.out.println(response);
             }
         }
         catch(FileNotFoundException e) {
